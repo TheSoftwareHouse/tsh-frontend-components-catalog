ed23937525847d1970e5d1512c32e00a49f0d9c1
Przemyslaw Bak <przemyslaw.bak@tsh.io>
2023-11-02 15:41:07 +0100

    feat: add missing dot files

---
+++ .storybook/main.ts
@@ -0,0 +1,57 @@
+import { mergeConfig } from 'vite';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { default as viteTsConfigPaths } from 'vite-tsconfig-paths';
+
+/** @type {import('@storybook/builder-vite').StorybookViteConfig} */
+module.exports = {
+  stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+  ],
+
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+
+  features: {
+    emotionAlias: false,
+    storyStoreV7: true,
+  },
+
+  async viteFinal(config, { configType }) {
+    /** @type {import('vite').UserConfig} */
+    const decoratedConfig = {
+      plugins: [viteTsConfigPaths()],
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+        esbuildOptions: {
+          define: {
+            global: 'globalThis',
+          },
+        },
+      },
+      build: {
+        rollupOptions: {
+          plugins: [rollupNodePolyFill()],
+        },
+      },
+    };
+
+    if (configType === 'DEVELOPMENT') {
+      decoratedConfig.build = {
+        ...decoratedConfig.build,
+      };
+    }
+
+    return mergeConfig(config, decoratedConfig);
+  },
+
+  docs: {
+    autodocs: true,
+  },
+};
